d1e94bb1da2471ae344e3b75acf2c6300f596d79
andreyhigher@gmail.com <andreyhigher@gmail.com>
2019-01-06 17:35:10 +0300

    Updated resender. Added reply, doc type in attachments

--- resender.py
+++ resender.py
@@ -2,7 +2,7 @@
 
 import os, configparser, telebot
 from vk_api.longpoll import VkLongPoll, VkEventType
-import vk_api, datetime, threading, re
+import vk_api, threading, re
 
 pwd = os.environ['HOME']+'/.config/vk_to_telegram'
 config = configparser.ConfigParser()
@@ -40,9 +40,31 @@ def ParseAtta(msg):
             attachments.append('photo {}'.format(url))
         elif i['type'] == 'audio_message':
             attachments.append('voice message '+i['audio_message']['link_ogg'])
+        elif i['type'] == 'doc':
+            attachments.append('document '+i['doc']['url'])
         else:
             attachments.append(str(i))
     return attachments
+
+def ParseRepl(msg, api, fwd_level=1):
+    bot_send('| '*(fwd_level-1)+'\\_'+'\tReply: ')
+    reply = msg['reply_message']
+    print(reply)
+    print()
+    print()
+    user = api.users.get(user_ids=reply['from_id'])[0]
+    bot_send('| '*fwd_level+'\tfrom "{} {}": '.format(user['first_name'], user['last_name']) + reply['text'])
+    if 'attachments' in reply.keys():
+        if reply['attachments']!=[]:
+            attachments = ParseAtta(reply)
+            bot_send('|'*fwd_level+'\tattachments: '+attachments[0])
+            for i in attachments[1:]:
+                bot_send(i)
+    if 'fwd_messages' in reply.keys():
+        if reply['fwd_messages']!=[]:
+            ParseForw(reply, api, fwd_level+1)
+    print()
+    print()
 
 def ParseForw(msg, api, fwd_level=1):
     bot_send('| '*(fwd_level-1)+'\\_'+'\tForwards: ')
@@ -51,16 +73,19 @@ def ParseForw(msg, api, fwd_level=1):
         print()
         print()
         user = api.users.get(user_ids=i['from_id'])[0]
-        bot_send('| '*fwd_level+'from "{} {}": '.format(user['first_name'], user['last_name']) + i['text'])
+        bot_send('| '*fwd_level+'\tfrom "{} {}": '.format(user['first_name'], user['last_name']) + i['text'])
         if 'attachments' in i.keys():
             if i['attachments']!=[]:
                 attachments = ParseAtta(i)
                 bot_send('|'*fwd_level+'\tattachments: '+attachments[0])
-                for i in attachments[1:]:
-                    bot_send(i)
+                for j in attachments[1:]:
+                    bot_send(j)
+        if 'reply_message' in i.keys():
+            if i['reply_message']!=[]:
+                ParseRepl(i, api, fwd_level+1)
         if 'fwd_messages' in i.keys():
-            if msg['fwd_messages']!=[]:
-                ParseForw(i, me, api, fwd_level+1)
+            if i['fwd_messages']!=[]:
+                ParseForw(i, api, fwd_level+1)
         print()
         print()
 
@@ -68,16 +93,17 @@ def ParsePriv(msg, me, user, api):
     print(msg)
     print()
     print()
-    user = api.users.get(user_ids=msg['from_id'])[0]
     bot_send('Sent from "{} {}" message to "{} {}": '.format(user['first_name'], user['last_name'], me['first_name'], me['last_name']) + msg['text'])
     if msg['attachments']!=[]:
         attachments = ParseAtta(msg)
         bot_send('Attachments: '+attachments[0])
         for i in attachments[1:]:
             bot_send(i)
-    if 'fwd_messages' in msg.keys():
-        if msg['fwd_messages']!=[]:
-            ParseForw(msg, api)
+    if 'reply_message' in msg.keys():
+        if msg['reply_message']!=[]:
+            ParseRepl(msg, api)
+    if msg['fwd_messages']!=[]:
+        ParseForw(msg, api)
     print()
     print()
 
@@ -92,6 +118,9 @@ def ParseChat(msg, me, user, api):
         bot_send('Attachments: '+attachments[0])
         for i in attachments[1:]:
             bot_send(i)
+    if 'reply_message' in msg.keys():
+        if msg['reply_message']!=[]:
+            ParseRepl(msg, api)
     if msg['fwd_messages']!=[]:
         ParseForw(msg)
     print()
@@ -110,6 +139,7 @@ class LongPool(threading.Thread):
         session = vk_api.VkApi(token=self.token, api_version='5.92')
         api = session.get_api()
         me = api.users.get()[0]
+        print('Bot have started for "{} {}"'.format(me['first_name'], me['last_name']))
         while 1:
             long_pooll = VkLongPoll(session)
             try:
@@ -117,16 +147,16 @@ class LongPool(threading.Thread):
                     if self.paused:
                         continue
                     if i.type == VkEventType.MESSAGE_NEW:
-                        msg_ext = api.messages.getById(message_ids=i.message_id, extended = 1)
+                        msg_ext = api.messages.getById(message_ids=i.message_id)
                         msg = msg_ext['items'][0]
-                        user = msg_ext['profiles'][0]
+                        user = api.users.get(user_ids=msg['from_id'])[0]
                         if msg['out']:
                             continue
-                        if msg['peer_id']-2000000000 in self.chats:
+                        if str(msg['peer_id']-2000000000) in self.chats:
                            ParseChat(msg, me, user, api)
-                        if '*' in self.chat_users:
+                        elif '*' in self.chat_users:
                             ParsePriv(msg, me, user, api)
-                        elif msg['from_id'] in self.chat_users:
+                        elif str(msg['from_id']) in self.chat_users:
                             ParsePriv(msg, me, user, api)
 
             except Exception as exep:
@@ -162,7 +192,7 @@ def kill(message):
 accounts = [i.strip("[]' ") for i in config.get('General', 'vk_users').split(',')]
 check_configs = []
 for name in accounts:
-    check_configs.append([config.get(name, 'token'), [int(i.strip("[] '")) for i in config.get(name, 'chat_users').split(',')], [int(i.strip("[] '")) for i in config.get(name, 'chats').split(',')]])
+    check_configs.append([config.get(name, 'token'), [i.strip("[] '") for i in config.get(name, 'chat_users').split(',')], [i.strip("[] '") for i in config.get(name, 'chats').split(',')]])
 
 global threads
 threads = []
@@ -173,7 +203,5 @@ for i in check_configs:
 while 1:
     try:
         bot.polling(none_stop=True)
-    except KeyboardInterrupt:
-        exit()
     except Exception as exep:
         bot_send('Ohhh... there are some errors: '+str(exep))
